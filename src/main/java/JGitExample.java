16cf5fdb58802eccdfe0129ad84820fb143c99c0
michael <michael@mac>
2023-03-03 01:19:12 +0100

    ...

--- src/main/java/JGitExample.java
+++ src/main/java/JGitExample.java
@@ -1,85 +1,106 @@
+import org.eclipse.jgit.api.*;
+import org.eclipse.jgit.api.errors.*;
+import org.eclipse.jgit.lib.*;
+import org.eclipse.jgit.storage.file.*;
+import org.eclipse.jgit.transport.*;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.dircache.DirCache;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import java.util.zip.*;
 
 public class JGitExample {
+
+    private static final String TIMESTAMP_FILE = "timestamp.txt";
+
     public static void main(String[] args) throws IOException, GitAPIException {
-        // Check that the correct number of arguments were passed
-        if (args.length != 1) {
-            System.out.println("Usage: JGitExample <path to zip file>");
+        if (args.length == 0) {
+            System.out.println("Please provide path to zip file containing the git repository");
             return;
         }
 
-        // Get the path to the zip file
-        String zipFilePath = args[0];
+        File zipFile = new File(args[0]);
+        File unzipDir = new File("/tmp/new");
 
-        // Unzip the repository to /tmp/new
-        Path tmpDir = Files.createTempDirectory("new");
-        unzip(zipFilePath, tmpDir.toString());
+        if (!zipFile.exists()) {
+            System.out.println("Zip file does not exist");
+            return;
+        }
 
-        // Get the path to the unzipped repository
-        String repoPath = tmpDir.resolve("repo.git").toString();
+        if (!unzipDir.exists()) {
+            unzipDir.mkdirs();
+        }
 
-        // Initialize a new repository at the unzipped repository's location
-        Repository repository = FileRepositoryBuilder.create(new File(repoPath));
+        try (ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(zipFile))) {
+            ZipEntry zipEntry = zipInputStream.getNextEntry();
 
-        // Create a new Git object from the repository
-        Git git = new Git(repository);
+            while (zipEntry != null) {
+                File file = newFile(unzipDir, zipEntry);
 
-        // Create an index for the bare repository
-        DirCache index = DirCache.newInCore();
-        index.lock();
-        repository.writeDirCache(index);
+                if (zipEntry.isDirectory()) {
+                    file.mkdirs();
+                } else {
+                    try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+                        byte[] buffer = new byte[1024];
+                        int length;
+                        while ((length = zipInputStream.read(buffer)) > 0) {
+                            fileOutputStream.write(buffer, 0, length);
+                        }
+                    }
+                }
 
-        // Add the file to the repository
-        git.add().addFilepattern(".").call();
+                zipEntry = zipInputStream.getNextEntry();
+            }
+        }
 
-        // Commit the changes to the repository
-        git.commit().setMessage("Initial commit").call();
+        File repoDir = new File(unzipDir, zipFile.getName().replaceAll(".zip", ""));
 
-        // Retrieve the HEAD commit
-        String head = repository.resolve("HEAD").getName();
+        try (Git git = Git.open(repoDir)) {
+            // Add or modify timestamp.txt
+            File timestampFile = new File(repoDir, TIMESTAMP_FILE);
+            if (timestampFile.exists()) {
+                // File already exists, modify it
+                try (FileWriter fileWriter = new FileWriter(timestampFile)) {
+                    fileWriter.write(String.valueOf(System.currentTimeMillis()));
+                }
+            } else {
+                // File doesn't exist, create it
+                timestampFile.createNewFile();
+                try (FileWriter fileWriter = new FileWriter(timestampFile)) {
+                    fileWriter.write(String.valueOf(System.currentTimeMillis()));
+                }
+                git.add().addFilepattern(TIMESTAMP_FILE).call();
+            }
 
-        // Print out the commit ID
-        System.out.println("Committed files to repository " + repository.getDirectory());
-        System.out.println("HEAD commit ID: " + head);
+            // Commit changes
+            git.commit().setMessage("Added or modified " + TIMESTAMP_FILE).call();
 
-        // Close the Git object and repository
-        git.close();
-        repository.close();
+            // Zip repository
+            File newZipFile = new File(zipFile.getAbsolutePath());
+            try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(newZipFile))) {
+                zipDir(repoDir, repoDir, zipOutputStream);
+            }
+        }
+
+        System.out.println("Done!");
     }
 
-    private static void unzip(String zipFilePath, String destDir) throws IOException {
-        File dest = new File(destDir);
-        byte[] buffer = new byte[1024];
-        ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFilePath));
-        ZipEntry zipEntry = zis.getNextEntry();
-        while (zipEntry != null) {
-            File newFile = newFile(dest, zipEntry);
-            if (zipEntry.isDirectory()) {
-                newFile.mkdirs();
+    private static void zipDir(File directoryToZip, File rootDirectory, ZipOutputStream zipOutputStream) throws IOException {
+        File[] children = directoryToZip.listFiles();
+        for (File file : children) {
+            if (file.isDirectory()) {
+                zipDir(file, rootDirectory, zipOutputStream);
             } else {
-                newFile.getParentFile().mkdirs();
-                FileOutputStream fos = new FileOutputStream(newFile);
-                int len;
-                while ((len = zis.read(buffer)) > 0) {
-                    fos.write(buffer, 0, len);
+                String relativePath = file.getAbsolutePath().substring(rootDirectory.getAbsolutePath().length() + 1);
+                ZipEntry zipEntry = new ZipEntry(relativePath);
+                zipOutputStream.putNextEntry(zipEntry);
+                try (FileInputStream fileInputStream = new FileInputStream(file)) {
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = fileInputStream.read(buffer)) > 0) {
+                        zipOutputStream.write(buffer, 0, length);
+                    }
                 }
-                fos.close();
+                zipOutputStream.closeEntry();
             }
-            zipEntry = zis.getNextEntry();
         }
-        zis.closeEntry();
-        zis.close();
     }
 
     private static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
@@ -90,5 +111,6 @@ public class JGitExample {
             throw new IOException("Entry is outside of the target dir: "+destFilePath);
         }
         return destFile;
-    }
+     }
 }
+   
